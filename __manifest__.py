52600593d9c3eef7beb396dca6677787b7b19a4a
Vladimir Kanchev <95029070+vladikanchev@users.noreply.github.com>
2025-06-04 11:19:02 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "HR Attendance Kiosk Limit",
+    "version": "1.0",
+    "category": "Human Resources",
+    "summary": "Limits Kiosk Mode Check-in and Check-out by time",
+    "author": "Custom",
+    "depends": ["hr_attendance"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
